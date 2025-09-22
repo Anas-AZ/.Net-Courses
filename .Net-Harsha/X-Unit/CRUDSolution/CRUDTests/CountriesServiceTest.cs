5e6fdcfe63822ac614f101963b76261eebfc5526
Anas Ahammed <anas.ahammed@firstscreen.com>
2025-09-22 18:21:20 +0530

    Add project files excluding .vs folder

---
+++ .Net-Harsha/X-Unit/CRUDSolution/CRUDTests/CountriesServiceTest.cs
@@ -0,0 +1,78 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using Services;
+
+
+namespace CRUDTests
+{
+    public class CountriesServiceTest
+    {
+        private readonly ICountriesService _countriesService;
+
+        public CountriesServiceTest()
+        {
+            _countriesService = new CountriesService();
+        }
+        //When CountruAddRequest is null, it should throw ArgumentNullException
+
+        [Fact]
+        public void AddCountry_NullCountry()
+        {
+            //Arrange
+            CountryAddRequest? request = null;
+            
+            //Assert
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                //Act
+                _countriesService.AddCountry(request);
+            });
+        }
+
+        //When the CountryName is null, it should throw argument exception
+        [Fact]
+        public void Country_CountryNameIsNull()
+        {
+            //Arrange
+            CountryAddRequest? request = new CountryAddRequest { CountryName = null};
+
+            //Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                //Act
+                _countriesService.AddCountry(request);
+            });
+        }
+        //When the countryname is duplicate, it should throw argument exception
+        [Fact]
+        public void Country_DuplicateCountryName()
+        {
+            //Arrange
+            CountryAddRequest? request1 = new CountryAddRequest { CountryName = "USA" };
+            CountryAddRequest? request2 = new CountryAddRequest { CountryName = "USA" };
+
+
+            //Assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                //Act
+                _countriesService.AddCountry(request1);
+                _countriesService.AddCountry(request2);
+
+            });
+        }
+        //When you supply proper countryname, it should insert (add) the country to the existing list of countries
+        [Fact]
+        public void AddCountry_ProperCountryDetails()
+        {
+            //Arrange
+            CountryAddRequest? request = new CountryAddRequest() { CountryName = "Japan"};
+
+            //Act
+            CountryResponse response =  _countriesService.AddCountry(request);
+
+            //Assert
+            Assert.True(response.CountryId != Guid.Empty);
+        }
+    }
+}
---
+++ .Net-Harsha/X-Unit/CRUDSolution/Services/CountriesService.cs
@@ -0,0 +1,49 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace Services
+{
+    public class CountriesService : ICountriesService
+    {
+        //private field
+        private readonly List<Country> _countries;
+
+        public CountriesService()
+        {
+            _countries = new List<Country>();
+        }
+
+        public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
+        {
+            //Validation: countryAddRequest parameter can't be null
+            if (countryAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(countryAddRequest));
+            }
+
+            //Validation: countryName can't be null
+            if (countryAddRequest.CountryName == null)
+            {
+                throw new ArgumentException(nameof(countryAddRequest.CountryName));
+            }
+
+            //Validation: countryNAme can't be duplicate
+            if(_countries.Where(temp => temp.CountryName == countryAddRequest.CountryName).Count() > 0)
+            {
+                throw new ArgumentException("Given country name already exists");
+            }
+
+            //Convert object from CountryAddRequest to Country type
+            Country country = countryAddRequest.ToCountry();
+
+            //generate CountryId
+            country.CountryId = Guid.NewGuid();
+
+            //Add country object into countries
+            _countries.Add(country);
+
+            return country.ToCountryResponse();
+        }
+    }
+}
